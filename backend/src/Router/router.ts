24207620d20237f36f35d7661f767b753d8ff4aa
Niraj Shah <nirajshah950@gmail.com>
2026-02-09 22:17:59 +0530

    call the user route in the Main router file.

--- backend/src/Router/router.ts
+++ backend/src/Router/router.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import { userRouter } from "../Utility/import";
 
 const attendenceRouter = Router();
+
+attendenceRouter.use(userRouter);
 
 export default attendenceRouter;
